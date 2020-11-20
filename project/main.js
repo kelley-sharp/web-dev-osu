af02dab828b53a8c3ec3c1c2b54e13f76c59fa7c
kelley <kelleylsharp@gmail.com>
2020-11-19 23:24:41 -0800

    installed ESlint

--- project/main.js
+++ project/main.js
@@ -3,8 +3,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
   const carouselPhotos = document.getElementsByClassName("carousel__photo");
   const totalCarouselPhotos = carouselPhotos.length;
-  const slide = 0;
-  const isMoving = true;
+  let slide = 0;
+  let isMoving = true;
 
   function setInitialClasses() {
     // Targets the previous, current, and forward items
@@ -54,18 +54,17 @@ window.addEventListener("DOMContentLoaded", (event) => {
   // Disable navigation (for when the carousel is moving)
   function disableNavigation() {
     // Set 'moving' to true for the duration of the slide transition (500ms)
-    moving = true;
+    isMoving = true;
     setTimeout(function () {
-      moving = false;
+      isMoving = false;
     }, 500);
   }
 
   function moveCarouselTo(slide) {
-
     // Default settings for middle slides
-    let oldBack = totalCarouselPhotos[slide - 1]
-    let oldForward = totalCarouselPhotos[slide + 1]
-    let newBack = totalCarouselPhotos[]
+    let oldBack = totalCarouselPhotos[slide - 1];
+    let oldForward = totalCarouselPhotos[slide + 1];
+    let newBack = totalCarouselPhotos[0];
     let newForward;
 
     // Check if slide is the first or the last or in between and update surrounding slides
@@ -84,7 +83,5 @@ window.addEventListener("DOMContentLoaded", (event) => {
     carouselPhotos[newBack].classList.add("back");
     carouselPhotos[slide].classList.add("active");
     carouselPhotos[newForward].classList.add("forward");
-
-
   }
 });
