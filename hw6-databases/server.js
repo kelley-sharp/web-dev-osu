5ecd3fc7c0aae1f4dee62be10469439a677b802c
Kelley Sharp <kelleylsharp@gmail.com>
2020-12-04 23:47:32 -0800

    hooking up database hopefully

--- hw6-databases/server.js
+++ hw6-databases/server.js
@@ -10,17 +10,26 @@ app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
-const workouts = [
-  {
-    name: "arm-thingy",
-    reps: 2,
-    weight: 25,
-    date: new Date(),
-    unit: "lbs",
-    id: 1,
-  },
-  { name: "squats", reps: 12, weight: 8, date: new Date(), unit: "lbs", id: 2 },
-];
+const mysql = require("mysql");
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "student",
+  password: "default",
+  database: "student",
+});
+
+// DUMMY TEST DATA
+// const workouts = [
+//   {
+//     name: "arm-thingy",
+//     reps: 2,
+//     weight: 25,
+//     date: new Date(),
+//     unit: "lbs",
+//     id: 1,
+//   },
+//   { name: "squats", reps: 12, weight: 8, date: new Date(), unit: "lbs", id: 2 },
+// ];
 
 // rendering the single page app handlebars page
 app.get("/", function (req, res) {
@@ -30,26 +39,69 @@ app.get("/", function (req, res) {
 // create a new workout
 app.post("/workouts", function (req, res) {
   const workout = req.body;
-  res.status(201).json({
-    workout,
-  });
+  mysql.pool.query(
+    "INSERT INTO workouts (`name`, `reps`, `weight`, `date`, `lbs`) VALUES (?)",
+    [
+      req.body.name,
+      req.body.reps,
+      req.body.weight,
+      req.body.date,
+      req.body.unit === "lbs",
+    ],
+    function (err, result) {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.status(201).json({
+        workout: result[0],
+      });
+    }
+  );
 });
 
 // edit a workout given the ID
 app.put("/workouts/:id", function (req, res) {
   const workoutId = req.params.id;
   const workout = req.body;
-  res.json({
-    workout,
-  });
+  mysql.pool.query(
+    "UPDATE workouts SET name=?, reps=?, weight=? date=? lbs=? WHERE id=? ",
+    [
+      req.body.name,
+      req.body.reps,
+      req.body.weight,
+      req.body.date,
+      req.body.unit === "lbs",
+      workoutId,
+    ],
+    function (err, result) {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.json({
+        workout: result[0],
+      });
+    }
+  );
 });
 
 // destroy a workout given the ID
 app.delete("/workouts/:id", function (req, res) {
   const workoutId = req.params.id;
-  res.json({
-    success: true,
-  });
+  mysql.pool.query(
+    "DELETE FROM workouts WHERE id=?",
+    [workoutId],
+    function (err, result) {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.json({
+        success: true,
+      });
+    }
+  );
 });
 
 app.use(function (req, res) {
@@ -59,6 +111,23 @@ app.use(function (req, res) {
 app.use(function (err, req, res, next) {
   res.status(500).json({
     error: err.message,
+  });
+});
+
+app.get("/reset-table", function (req, res, next) {
+  mysql.pool.query("DROP TABLE IF EXISTS workouts", function (err) {
+    //replace your connection pool with the your variable containing the connection pool
+    const createString =
+      "CREATE TABLE workouts(" +
+      "id INT PRIMARY KEY AUTO_INCREMENT," +
+      "name VARCHAR(255) NOT NULL," +
+      "reps INT," +
+      "weight INT," +
+      "date DATE," +
+      "lbs BOOLEAN)";
+    mysql.pool.query(createString, function (err) {
+      res.render("home", { workouts: [] });
+    });
   });
 });
 
