264d398c59dc79f6af8b2f7a9d7bb236571bfe5d
Kelley Sharp <kelleylsharp@gmail.com>
2020-12-09 22:50:58 -0800

    added closure loop comparison

--- a4-fixing-closure-loop.js
+++ a4-fixing-closure-loop.js
@@ -20,3 +20,24 @@ function testList() {
 }
 
 console.log(testList());
+
+// previously
+
+// function buildList(list) {
+//   var result = [];
+//   for (var i = 0; i < list.length; i++) {
+//       var item = 'item' + list[i];
+//       result.push( function() {alert(item + ' ' + list[i])} );
+//   }
+//   return result;
+// }
+
+// function testList() {
+//   var fnlist = buildList([1,2,3]);
+//   // using j only to help prevent confusion - could use i
+//   for (var j = 0; j < fnlist.length; j++) {
+//       fnlist[j]();
+//   }
+// }
+
+//returns: item3 undefined, three times
